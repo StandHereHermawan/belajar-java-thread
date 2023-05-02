2e8c4fefbc744a68feb96b2e77c5c40048354dd5
StandHereHermawan <nolifegansaya@gmail.com>
2023-05-02 18:32:54 +0700

    Java Thread CompletableFuture Test

--- src/test/java/arief/belajar/java/thread/CompletableFutureTest.java
+++ src/test/java/arief/belajar/java/thread/CompletableFutureTest.java
@@ -2,19 +2,22 @@ package arief.belajar.java.thread;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Random;
 import java.util.concurrent.*;
 
 public class CompletableFutureTest {
 
     private ExecutorService executorService = Executors.newFixedThreadPool(10);
 
-    public Future<String> getValue(){
+    private Random random = new Random();
+
+    public CompletableFuture<String> getValue(){
         CompletableFuture<String> future = new CompletableFuture<>();
 
         executorService.execute(() -> {
             try {
                 Thread.sleep(1000);
-                future.complete("Selesai");
+                future.complete("Arief Karditya Hermawan");
             } catch (InterruptedException e) {
                 future.completeExceptionally(e);
             }
@@ -27,5 +30,45 @@ public class CompletableFutureTest {
     void create() throws ExecutionException, InterruptedException {
         Future<String> future = getValue();
         System.out.println(future.get());
+    }
+
+    private void execute(CompletableFuture<String> future, String value) {
+        executorService.execute(() -> {
+            try {
+                Thread.sleep(1000);
+                future.complete(value);
+            } catch (InterruptedException e) {
+                future.completeExceptionally(e);
+            }
+        });
+    }
+
+    public Future<String> getFastest(){
+        CompletableFuture<String> future = new CompletableFuture<>();
+
+        execute(future,"Thread 1");
+        execute(future,"Thread 2");
+        execute(future,"Thread 3");
+
+        return future;
+    }
+
+    @Test
+    void testFastest() throws ExecutionException, InterruptedException {
+        System.out.println(getFastest().get());
+    }
+
+    @Test
+    void completionStage() throws ExecutionException, InterruptedException {
+        CompletableFuture<String> future = getValue();
+
+        CompletableFuture<String[]> future2 = future
+                .thenApply(s -> s.toUpperCase())
+                .thenApply(s -> s.split(" "));
+
+        String[] strings = future2.get();
+        for (var value: strings) {
+            System.out.println(value);
+        }
     }
 }
